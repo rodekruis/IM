675f26b3c063b2e46677c154a99bc786eee49c32
rodekruis <mvanderveen@redcross.nl>
2014-06-10 07:10:10 +0200

    change to running production minified versions

--- config/env/production.js
+++ config/env/production.js
@@ -4,22 +4,59 @@ var secrets = require('../secrets');
 
 module.exports = {
 	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost/im-dev',
+	app: {
+		title: 'Rode Kruis Digital Operations Centre'
+	},
 	assets: {
 		lib: {
 			css: [
 				'public/lib/bootstrap/dist/css/bootstrap.min.css',
 				'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
+				'public/lib/leaflet-dist/leaflet.css',
+				'public/lib/cartodb/cartodb.css',
+				'public/lib/jquery-ui/themes/smoothness/jquery-ui.min.css',
+				'public/lib/angular-centered/angular-centered.css',
+				'public/lib/angular-ui-dashboard/dist/angular-ui-dashboard.css',
+				'public/lib/ng-grid/ng-grid.min.css',
+				'public/lib/select2/select2.css',
+				//'public/lib/select2-bootstrap-css/select2-bootstrap.css',
+				'public/lib/forms-angular/css/forms-angular-bs3.less',
+				'public/lib/angular-carousel/dist/angular-carousel.min.css'
 			],
 			js: [
+				'public/lib/jquery/jquery.min.js',
+				'public/lib/jquery-ui/ui/minified/jquery-ui.min.js',
+				'public/lib/lodash/dist/lodash.min.js',
 				'public/lib/angular/angular.min.js',
+				'public/lib/angular-lodash/angular-lodash.js',
+				'public/lib/angular-route/angular-route.min.js',
 				'public/lib/angular-resource/angular-resource.min.js', 
-				'public/lib/angular-cookies/angular-cookies.min.js',  
+				'public/lib/angular-cookies/angular-cookies.min.js',    
 				'public/lib/angular-animate/angular-animate.min.js', 
-				'public/lib/angular-touch/angular-touch.min.js', 
+				'public/lib/angular-touch/angular-touch.min.js',  
 				'public/lib/angular-sanitize/angular-sanitize.min.js', 
 				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
 				'public/lib/angular-ui-utils/ui-utils.min.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js'
+				'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
+				'public/lib/angular-leaflet/dist/angular-leaflet-directive.min.js',
+				'public/lib/cartodb/cartodb.js',
+				'public/lib/cartodb/activelayers.js',
+				'public/lib/angular-centered/angular-centered.js',
+				'public/lib/angular-deckgrid/angular-deckgrid.js',
+				//'public/lib/smart-table/Smart-Table.min.js',
+				'public/lib/angular-ui-sortable/sortable.min.js',
+				'public/lib/angular-ui-dashboard/dist/angular-ui-dashboard.js',
+				'public/lib/angular-ui-date/src/date.js',
+				'public/lib/angular-ui-bootstrap-bower/ui-bootstrap-tpls.min.js',
+				'public/lib/ngInfiniteScroll/build/ng-infinite-scroll.min.js',
+				'public/lib/jspdf/dist/jspdf.min.js',
+				'public/lib/ng-grid/build/ng-grid.min.js',
+				'public/lib/angular-elastic/elastic.js',
+				'public/lib/angular-ui-select2/src/select2.js',
+				'public/lib/ckeditor/ckeditor.js',
+				'public/lib/ng-ckeditor/ng-ckeditor.min.js',
+				'public/lib/forms-angular/forms-angular.min.js',
+				'public/lib/angular-carousel/dist/angular-carousel.min.js'
 			]
 		},
 		css: 'public/dist/application.min.css',
