bdf05ca2e0ac7fe802f25b3a6814e365f004c3c2
rodekruis <mvanderveen@redcross.nl>
2014-10-21 19:41:49 +0200

    change login procedure

--- config/strategies/local.js
+++ config/strategies/local.js
@@ -5,7 +5,8 @@
  */
 var passport = require('passport'),
 	LocalStrategy = require('passport-local').Strategy,
-	User = require('mongoose').model('User');
+	User = require('mongoose').model('User'),
+	secrets = require('../secrets');
 
 module.exports = function() {
 	// Use local strategy
@@ -14,25 +15,44 @@ module.exports = function() {
 			passwordField: 'password',
 		},
 		function(email, password, done) {
-			User.findOne({
-				email: email
-			}, function(err, user) {
-				if (err) {
-					return done(err);
-				}
-				if (!user) {
-					return done(null, false, {
-						message: 'Unknown user'
-					});
-				}
-				if (!user.authenticate(password)) {
-					return done(null, false, {
-						message: 'Invalid password'
-					});
-				}
+			
+			var domainRegex = /@(.*$)/i;
+			
+			try {
+				// get domain from user email address in azure provided data
+				var matches = email.match(domainRegex);
+				var domain = matches[1].toLowerCase();
 				
-				return done(null, user);
-			});
+				// test if email is within valid domain in secrets.json
+				if (secrets.domains.indexOf(domain) > -1) {
+					User.findOne({
+						email: email
+					}, function(err, user) {
+						if (err) {
+							return done(err);
+						}
+						if (!user) {
+							return done(null, false, {
+								message: 'Unknown user'
+							});
+						}
+						if (!user.authenticate(password)) {
+							return done(null, false, {
+								message: 'Invalid password'
+							});
+						}
+						
+						return done(null, user);
+					});
+				}else{
+					// fail        
+					done(new Error('Dit is geen geldig domein waarmee u kunt inloggen'));
+				 }
+			}
+					
+			catch (e) {
+				console.log(e);
+			}			
 		}
 	));
 };
